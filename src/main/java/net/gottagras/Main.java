b736a2cbddf92ed0a031aec140d77b66a567fdc2
Loris Sogliuzzo <loris@sogliuzzo.com>
2026-01-10 18:34:02 +0100

    Add Spigot repository and API dependency; refactor Main class to extend JavaPlugin

--- src/main/java/net/gottagras/Main.java
+++ src/main/java/net/gottagras/Main.java
@@ -1,7 +1,15 @@
 package net.gottagras;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin disabled!");
     }
 }
